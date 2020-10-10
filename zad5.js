f673b9fb62b47843849d4147833afc44509fc11f
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2020-10-10 21:58:42 +0200

    psm20 / lab02 / z05

---
+++ zad5.js
@@ -0,0 +1,23 @@
+const collections = [
+    {},
+    15,
+    "hello@test.pl",
+    null,
+    ['aaa', 'bbb', 5],
+    'admin@gmail.com',
+    undefined,
+    'a34@yahoo.com',
+    '321@a',
+    '321.pl'
+];
+
+const isMail = (email) => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
+}
+
+const getMails = (collections) => {
+    return collections.filter(value => isMail(value)).sort();
+};
+
+console.log(getMails(collections));
