db008c655ce650e78f120a589c650d96630c3a90
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2020-10-15 01:57:30 +0200

    psm20 / lab03 / z03

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {TouchableOpacity} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  state = {
+    isEnabled: false
+  };
+
+  handleClick = () => {
+    this.setState({
+      ...this.state,
+      isEnabled: !this.state.isEnabled
+    })
+  };
+
+  render() {
+    const { isEnabled } = this.state;
+
+    return (
+        <View style={styles.container}>
+          <Text style={styles.heading}>Zadanie 2</Text>
+
+          <TouchableOpacity style={styles.button} onPress={this.handleClick}>
+            <Text>{isEnabled ? "Ukryj" : "Pokaż"}</Text>
+          </TouchableOpacity>
+
+          {isEnabled && <><Text style={styles.text}>Nazywam się</Text><Text style={styles.textBold}>Mateusz Janczak</Text></>}
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#eee"
   },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    margin: 20
+  },
+  heading: {
+    fontSize: 20
+  },
+  text: {
+    fontSize: 16
+  },
+  textBold: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
 });
