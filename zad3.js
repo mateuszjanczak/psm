50dbbec5544cb1937ecc6dc7c6f7c3d03a68ac74
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2020-10-14 23:05:08 +0200

    psm20 / lab02 / z03 - FIX

--- zad3.js
+++ zad3.js
@@ -23,20 +23,14 @@ const user = {
 const calculateWeightedAverage = (user) => {
     const { name, surname, allGrades } = user;
 
-    const subjectGrades = allGrades.map(value => {
-        const { weight, grades } = value;
-        const { length } = grades;
+    const avgSubjectGrades = allGrades.map(({ subjectName, grades, weight }) => {
+        const avg = grades.reduce((acc, elem) => acc + elem, 0) / grades.length;
+        return { subjectName, avg, weight };
+    })
 
-        const sum = grades.reduce((acc, elem) => acc + elem);
+    const avgUser = avgSubjectGrades.reduce((acc, { avg, weight }) => acc + avg * weight, 0) / avgSubjectGrades.reduce((acc, { weight }) => acc + weight, 0);
 
-        return { sum, weight, length };
-    });
-
-    const numerator = subjectGrades.map(value => value.sum * value.weight).reduce((acc, elem) => acc + elem);
-    const denominator = subjectGrades.map(value => value.weight * value.length).reduce((acc, elem) =>  acc + elem);
-    const avg = numerator / denominator;
-
-    console.log(`Average for ${name} ${surname} is ${avg}`)
+    console.log(`Average for ${name} ${surname} is ${avgUser}`);
 }
 
 calculateWeightedAverage(user);
